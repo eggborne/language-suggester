4417e2b36ced76651f30a940b98deb337249fc1c
Mike Donovan <mike@mikedonovan.dev>
2023-01-13 12:32:09 -0800

    Add yes/no toggles, basic design of form rows

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
-window.onload = function() {
-  document.getElementById('settings-toggle').onpointerdown = function(e) {
-    console.log('blarg', e.target)
+window.onload = function () {
+  document.getElementById('settings-toggle').onpointerdown = function (e) {
     e.target.classList.toggle('open');
     document.querySelector('menu').classList.toggle('open');
-  }
+  };
+  [...document.getElementsByClassName('toggle')].forEach(function (toggle) {
+    toggle.onpointerdown = function(e) {
+      e.target.classList.toggle('yes');
+    };
+  });
 }
